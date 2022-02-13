b010f5bfeca8ea091960dac51e2aebc43c65f454
ConsultancyProjects <96533206+ConsultancyProjects@users.noreply.github.com>
2022-02-13 18:36:16 +0000

    Update environment.prod.ts
    crcted ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { ApplicationEnvironmentConfig } from "./default.environment";
 
 export const environment: IEnvironment = {
   ...ApplicationEnvironmentConfig,
-  apiHostURL: 'http://3.144.95.206:8081',
+  apiHostURL: 'http://18.221.235.136:8081',
   production: true
 };
